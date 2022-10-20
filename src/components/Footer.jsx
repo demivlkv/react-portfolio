662c3b0440a9c5d1eb8220fec02c2dc602403035
Demi Hayashi <demi.h@me.com>
2022-10-20 14:35:22 -0700

    create responsive social media icons in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { CgHeart, CgCopyright } from 'react-icons/cg';
+import { FiGithub, FiLinkedin, FiInstagram, FiMail } from 'react-icons/fi';
 
 const Footer = () => {
   return (
     <footer className="w-full p-8 text-[#8aa2a9] text-center">
         <p>Made with <CgHeart className="inline-flex pb-[3px]" style={{ color: '#f27c61' }} size={20} /> by Demi Hayashi.</p>
-        <p><CgCopyright className="inline-flex pb-[3px]" style={{ color: '#f27c61' }} size={20} /> 2022. All rights reserved.</p>
+        <p><CgCopyright className="inline-flex pb-[3px]" style={{ color: '#f27c61' }} size={20} /> 2022. All rights reserved. </p>
+        
+        {/* SOCIAL MEDIA ICONS */}
+        <div>
+          <ul className="lg:hidden sm:flex flex-row justify-center">
+            <li className="p-3 inline-flex"><a className="text-[#8aa2a9] hover:text-[#f27c61] hover:border-0" href="https://github.com/demivlkv" target="_blank"><FiGithub size={25} /></a></li>
+            <li className="p-3 inline-flex"><a className="text-[#8aa2a9] hover:text-[#f27c61] hover:border-0" href="https://www.linkedin.com/in/demihayashi/" target="_blank"><FiLinkedin size={25} /></a></li>
+            <li className="p-3 inline-flex"><a className="text-[#8aa2a9] hover:text-[#f27c61] hover:border-0" href="https://www.instagram.com/hii.demii/" target="_blank"><FiInstagram size={25} /></a></li>
+            <li className="p-3 inline-flex"><a className="text-[#8aa2a9] hover:text-[#f27c61] hover:border-0" href="mailto:hayashi.demi@gmail.com"><FiMail size={25} /></a></li>
+          </ul>
+        </div>
     </footer>
   );
 };
