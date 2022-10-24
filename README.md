1f7d9d4263b04c65e5935cfcf87ed949d7b4e052
Demi Hayashi <demi.h@me.com>
2022-10-24 14:59:28 -0700

    protect contact form id via dotenv; update README.md

--- README.md
+++ README.md
@@ -7,5 +7,21 @@ The intent for this project was to build an updated web developer portfolio util
 ### Built With:
 - [React.js](https://reactjs.org/)
 - [Framer-Motion](https://www.framer.com/motion/)
+- [React Icons](https://react-icons.github.io/react-icons/)
 - JavaScript
-- [Tailwind CSS](https://tailwindcss.com/)
+- CSS / [Tailwind CSS](https://tailwindcss.com/)
+- HTML
+
+## Installation
+1. To install this application, `git clone` this repository, or download the `.zip` file in the dropdown after clicking the green ‘Code’ button at the top of this repository—as long as it is in your local branch.
+2. Ensure Node.js is installed in your operating system.
+3. Install ***all*** of the npm packages listed above via command-line in terminal by entering `npm install`.
+4. Sign up for a free [getform.io](https://getform.io/) account, and enter your unique form endpoint.
+
+## Usage
+Using this application is fairly simple. Enter `npm start` at the root directory in terminal. This will start up the React app, and opens the application automatically in your browser.
+
+## Questions
+For any questions about this repository, please contact me at [demi.h@me.com](mailto:demi.h@me.com).
+
+To view more of my works, please visit my GitHub: [demivlkv](https://github.com/demivlkv).
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,6 +8,8 @@ const Contact = () => {
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
+    const formId = process.env.REACT_APP_CONTACT_FORM_ID;
+    const formUrl = 'https://getform.io/f/' + formId;
 
     // handles form validation
     function handleChange(e) {
@@ -33,87 +35,80 @@ const Contact = () => {
         }
     };
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(formState);
-    };
-
     return (
-    <Animation>
-    <div name="contact" className="w-full md:h-screen flex justify-center items-center mx-auto p-4">
-        <div className="max-w-screen-lg w-full grid grid-cols-1 sm:grid-cols-2 gap-4">
-        
-            <div className="sm:text-right">
-                <h2>
-                    Contact
-                </h2>
-            </div>
-            <div></div>
+        <Animation>
+            <div name="contact" className="w-full md:h-screen flex justify-center items-center mx-auto p-4">
+                <div className="max-w-screen-lg w-full grid grid-cols-1 sm:grid-cols-2 gap-4">
+                
+                    <div className="sm:text-right">
+                        <h2>Contact</h2>
+                    </div>
+                    <div></div>
 
-            <div>
-                <p className="py-4">Please feel free to reach out with any questions or opportunities. I look forward to hearing from you!</p>
+                    <div>
+                        <p className="py-4">Please feel free to reach out with any questions or opportunities. I look forward to hearing from you!</p>
 
-                <div className="max-w-[75%] w-full mx-auto grid grid-cols-2 gap-2">
-                    <div className="inline-flex items-center">
-                        <FiMapPin style={{ color: '#f27c61' }} size={20} />&nbsp;Location
+                        <div className="max-w-[75%] w-full mx-auto grid grid-cols-2 gap-2">
+                            <div className="inline-flex items-center">
+                                <FiMapPin style={{ color: '#f27c61' }} size={20} />&nbsp;Location
+                            </div>
+                            <div className="inline-flex justify-end items-center">
+                                : Los Angeles, CA
+                            </div>
+                            <div className="inline-flex items-center">
+                                <FiAtSign style={{ color: '#f27c61' }} size={20} />&nbsp;E-mail
+                            </div>
+                            <div className="inline-flex justify-end items-center">
+                                :&nbsp;<a href="mailto:hayashi.demi@gmail.com">hayashidemi@gmail.com</a>
+                            </div>
+                            <div className="inline-flex items-center">
+                                <FiGithub style={{ color: '#f27c61' }} size={20} />&nbsp;GitHub
+                            </div>
+                            <div className="inline-flex justify-end items-center">
+                                :&nbsp;<a href="https://github.com/demivlkv" target="_blank">@demivlkv</a>
+                            </div>
+                            <div className="inline-flex items-center">
+                                <IoMdGlobe style={{ color: '#f27c61' }} size={20} />&nbsp;Languages
+                            </div>
+                            <div className="inline-flex justify-end items-center">
+                                : English, Japanese
+                            </div>
+                        </div>
                     </div>
-                    <div className="inline-flex justify-end items-center">
-                        : Los Angeles, CA
+
+                    {/* CONTACT FORM */}
+                    <div>
+                        <form method="POST" action={formUrl} id="contact-form" className="flex flex-col justify-center max-w-[600px] w-full">
+                            <div class="w-[80%] mx-auto">
+                                <label className="block text-[#f27c61] font-['Source_Code_Pro'] font-medium">Name:</label>
+                                <input type="text" defaultValue={name} onBlur={handleChange} name="name" className="w-full my-2 p-2 rounded bg-[#5e708964] border border-[#5e7089]" />
+
+                                <label className="block text-[#f27c61] font-['Source_Code_Pro'] font-medium">E-mail:</label>
+                                <input type="email" defaultValue={email} onBlur={handleChange} name="email" className="w-full my-2 p-2 rounded bg-[#5e708964] border border-[#5e7089]" />
+
+                                <label className="block text-[#f27c61] font-['Source_Code_Pro'] font-medium">Message:</label>
+                                <textarea name="message" defaultValue={message} onBlur={handleChange} rows="4" className="w-full my-2 p-2 rounded bg-[#5e708964] border border-[#5e7089]" />
+
+                                {errorMessage && (
+                                    <div>
+                                        <p className="error-text">{errorMessage}</p>
+                                    </div>
+                                )}
+
+                                <button type="submit" className="group w-[130px] mx-auto flex items-center my-2 pl-6 py-2 border-2 border-gray-300 rounded hover:bg-[#f27c611e] hover:border-[#eeb1a3]">
+                                    Submit
+                                    <span className="group-hover:ml-1 duration-300">
+                                        <FiChevronsRight className="ml-2" />
+                                    </span>
+                                </button>
+                            </div>
+                        </form>
                     </div>
-                    <div className="inline-flex items-center">
-                        <FiAtSign style={{ color: '#f27c61' }} size={20} />&nbsp;E-mail
-                    </div>
-                    <div className="inline-flex justify-end items-center">
-                        :&nbsp;<a href="mailto:hayashi.demi@gmail.com">hayashidemi@gmail.com</a>
-                    </div>
-                    <div className="inline-flex items-center">
-                        <FiGithub style={{ color: '#f27c61' }} size={20} />&nbsp;GitHub
-                    </div>
-                    <div className="inline-flex justify-end items-center">
-                        :&nbsp;<a href="https://github.com/demivlkv" target="_blank">@demivlkv</a>
-                    </div>
-                    <div className="inline-flex items-center">
-                        <IoMdGlobe style={{ color: '#f27c61' }} size={20} />&nbsp;Languages
-                    </div>
-                    <div className="inline-flex justify-end items-center">
-                        : English, Japanese
-                    </div>
+
                 </div>
             </div>
-
-            {/* CONTACT FORM */}
-            <div>
-                <form method="POST" action="https://getform.io/f/334ec745-eba1-4f48-a06a-ba6ee7e3fd13" id="contact-form" onSubmit={handleSubmit} className="flex flex-col justify-center max-w-[600px] w-full">
-                    <div class="w-[80%] mx-auto">
-                        <label className="block text-[#f27c61] font-['Source_Code_Pro'] font-medium">Name:</label>
-                        <input type="text" defaultValue={name} onBlur={handleChange} name="name" className="w-full my-2 p-2 rounded bg-[#5e708964] border border-[#5e7089]" />
-
-                        <label className="block text-[#f27c61] font-['Source_Code_Pro'] font-medium">E-mail:</label>
-                        <input type="email" defaultValue={email} onBlur={handleChange} name="email" className="w-full my-2 p-2 rounded bg-[#5e708964] border border-[#5e7089]" />
-
-                        <label className="block text-[#f27c61] font-['Source_Code_Pro'] font-medium">Message:</label>
-                        <textarea name="message" defaultValue={message} onBlur={handleChange} rows="4" className="w-full my-2 p-2 rounded bg-[#5e708964] border border-[#5e7089]" />
-
-                        {errorMessage && (
-                            <div>
-                                <p className="error-text">{errorMessage}</p>
-                            </div>
-                        )}
-
-                        <button type="submit" className="group w-[130px] mx-auto flex items-center my-2 pl-6 py-2 border-2 border-gray-300 rounded hover:bg-[#f27c611e] hover:border-[#eeb1a3]">
-                            Submit
-                            <span className="group-hover:ml-1 duration-300">
-                                <FiChevronsRight className="ml-2" />
-                            </span>
-                        </button>
-                    </div>
-                </form>
-            </div>
-
-        </div>
-    </div>
-    </Animation>
-  );
+        </Animation>
+    );
 };
 
 export default Contact;
