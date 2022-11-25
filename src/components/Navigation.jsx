21fc476089fbcaea6c6040800428db482dcc5c66
Demi Hayashi <106700986+demivlkv@users.noreply.github.com>
2022-11-25 10:50:09 -0800

    update Navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,29 +12,29 @@ const Navigation = (props) => {
   return (
     <div>
       {/* NAVBAR AT 768PX & ABOVE */}
-      <ul className="hidden font-['Source_Code_Pro'] md:flex">
+      <ul className="hidden font-normal uppercase tracking-wider md:flex">
         {pages.map((page) => (
           <li className={
-            props.currentPage.page === page ? 'mx-2 active font-semibold' : 'mx-2 font-semibold transition-all'
+            props.currentPage.page === page ? 'mx-2 active' : 'mx-2'
             } key={page} >
             <span onClick={() => props.setCurrentPage(page)} className={
-              props.currentPage === page ? 'mx-2 active font-semibold' : 'mx-2 font-medium hover:border-b-4 hover:border-[#5e7089] transition-all hover:text-white'
+              props.currentPage === page ? 'mx-2 active' : 'mx-2 hover:border-b-4 hover:border-[#2e4057] transition-all ease-in duration-300 hover:text-white'
             }>{page}</span>
           </li>
         ))}
       </ul>
 
       {/* HAMBURGER MENU AT 768PX & LOWER */}
-      <div onClick={toggleNavbar} className="md:hidden font-['Source_Code_Pro'] z-50">
-        {!nav ? <FiMenu size={30} className="transition-all hover:text-[#f27c61] hover:cursor-pointer" /> : <FiX size={30} className="transition-all hover:text-[#f27c61] hover:cursor-pointer" />}
+      <div onClick={toggleNavbar} className="md:hidden font-normal uppercase tracking-wider z-50">
+        {!nav ? <FiMenu size={30} className="transition-all ease-in duration-300 hover:text-[#79fcca] hover:cursor-pointer" /> : <FiX size={30} className="transition-all ease-in duration-300 hover:text-[#79fcca] hover:cursor-pointer" />}
 
-        <ul className={!nav ? "hidden" : "absolute top-16 left-0 w-full bg-[#2e4057] flex flex-col justify-center items-center"}>
+        <ul className={!nav ? "hidden" : "absolute top-16 left-0 w-full bg-[#242d39] flex flex-col justify-center items-center"}>
           {pages.map((page) => (
             <li onClick={toggleNavbar} className={
               currentPage.page === page ? 'text-[#edf6f9]' : 'my-2 transition-all'
               } key={page} >
             <span onClick={() => props.setCurrentPage(page)} className={
-              props.currentPage === page ? 'mx-2 active font-semibold' : 'mx-2 font-semibold transition-all ease-in hover:border-b-4 hover:border-[#5e7089] hover:text-white'
+              props.currentPage === page ? 'mx-2 active' : 'mx-2 transition-all ease-in duration-300 hover:border-b-4 hover:border-[#2e4057] hover:text-white'
             }>{page}</span>
             </li>
           ))}
